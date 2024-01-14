d71ad930f56c734c1d13eea64e6feec7f3bde279
loanthiho <loan.hothi24@student.passerellesnumeriques.org>
2024-01-14 16:36:24 +0700

    VND-12 <initial set up>

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>VND WEB</h1>
+      <h1>Vietnam Delicacies WEB</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
