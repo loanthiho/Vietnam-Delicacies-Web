b13fbe955356389659649023b51d75a636c75c80
HoVanDi <hovandideveloper@gmail.com>
2024-02-20 16:03:19 +0700

    fix: update code Home page web

--- Web/src/components/index.jsx
+++ Web/src/components/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Banner from "./Banner";
 import MainContent from "./MainContent";
 import styles from "./style.module.css";
@@ -7,7 +6,6 @@ const Page = () => {
   return (
     <>
       <Banner></Banner>
-
       <section className={styles.section_content}>
         <MainContent></MainContent>
       </section>
