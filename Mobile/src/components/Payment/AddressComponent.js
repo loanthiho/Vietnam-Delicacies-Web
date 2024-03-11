c9b789dcfd9668b58527740111de7924d3359642
thibackend <thi.a24technology@gmail.com>
2024-03-11 10:33:00 +0700

    integrate api with payment method

--- Mobile/src/components/Payment/AddressComponent.js
+++ Mobile/src/components/Payment/AddressComponent.js
@@ -4,7 +4,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { getUserAccessToken } from '../../api/storage';
 
-const AddressComponent = ({ navigation }) => {
+const AddressComponent = ({ navigation, userInfo, setUserInfo }) => {
   const [oldUserData, setOldUserData] = useState({});
   const getUserData = async () => {
     const userATK = await getUserAccessToken();
