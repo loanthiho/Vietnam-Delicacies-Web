8e9646c3dc49b87fa9165350971cc310b49dae96
thibackend <thi.a24technology@gmail.com>
2024-03-17 22:13:23 +0700

    Build api-order: getAll order by status, update order status

--- Server/controllers/orderCtl.js
+++ Server/controllers/orderCtl.js
@@ -1,11 +1,22 @@
-const { createNewOrder, getAllOrder } = require('../services/oderServices');
+const {
+    createNewOrder,
+    getAllOrder,
+    sellerGetAllOrder,
+    commonGetAllOrder,
+    updateOrderStatus } = require('../services/oderServices');
 
 const create = (req, res, next) => createNewOrder(req, res, next);
+const commonGetAll = (req, res, next) => commonGetAllOrder(req, res, next);
 const getAll = (req, res, next) => getAllOrder(req, res, next);
+const sellerGetAll = (req, res, next) => sellerGetAllOrder(req, res, next);
+const updateStatus = (req, res, next) => updateOrderStatus(req, res, next);
 
 
 
 module.exports = {
+    commonGetAll,
     create,
-    getAll
+    getAll,
+    sellerGetAll,
+    updateStatus
 }
--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -5,5 +5,9 @@ const { checkAuth } = require('../middleware/check-auth');
 
 
 router.post('/', checkAuth, orderCtl.create);
+
+// router.get('/', checkAuth, orderCtl.commonGetAll);
 router.get('/', checkAuth, orderCtl.getAll);
+router.get('/seller', checkAuth, orderCtl.sellerGetAll);
+router.patch('/:id', checkAuth, orderCtl.updateStatus);
 module.exports = router;
