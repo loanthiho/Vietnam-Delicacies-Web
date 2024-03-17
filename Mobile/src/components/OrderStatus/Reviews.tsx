379f966affc32cf6032ab4dfc9dbececa13ddcbb
thibackend <thi.a24technology@gmail.com>
2024-03-18 06:34:34 +0700

    VND-185 <Integrate api user get order and update order status>

--- Mobile/src/components/OrderStatus/Reviews.tsx
+++ Mobile/src/components/OrderStatus/Reviews.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -7,73 +7,63 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from 'react-native';
-import {useNavigation} from '@react-navigation/native'; 
+import { useNavigation } from '@react-navigation/native';
+import LoaderKit from 'react-native-loader-kit';
+import { useQuery } from '@tanstack/react-query';
+import api from '../../api/request';
 
 const Review = () => {
-  const navigation = useNavigation(); 
+  const navigation = useNavigation();
+  const { data, isLoading } = useQuery({
+    queryKey: ['get_order_CHO_DANH_GIA'],
+    queryFn: async () => {
+      const res = await api.get('orders', { params: { status: "CHO_DANH_GIA" } });
+      if (res) {
+        return res.data?.data;
+      }
+    },
+  });
 
-  const [cartItems, setCartItems] = useState([
-    {
-      id: 1,
-      name: 'Nem chua Thanh Hoá',
-      price: 150000,
-      Files: [
-        {
-          src: 'https://i.pinimg.com/564x/6a/9a/12/6a9a122a60a435725152db7a6632da58.jpg',
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: 'Gạo đen Tây Bắc',
-      price: 300000,
-      Files: [
-        {
-          src: 'https://i.pinimg.com/736x/8d/98/1e/8d981eadabf77f64baad46aac7279241.jpg',
-        },
-      ],
-    },
-    {
-      id: 3,
-      name: 'Gạo đen Tây Bắc',
-      price: 300000,
-      Files: [
-        {
-          src: 'https://i.pinimg.com/736x/8d/98/1e/8d981eadabf77f64baad46aac7279241.jpg',
-        },
-      ],
-    },
-  ]);
-
-  const renderItem = ({item}: any) => (
+  const renderItem = ({ item }: any) => (
     <View key={item.id} style={styles.itemContainer}>
-      <Image source={{uri: item.Files?.[0]?.src}} style={styles.itemImage} />
+      <Image source={{ uri: item.Product?.Files?.[0]?.src }} style={styles.itemImage} />
       <View style={styles.content}>
-        <Text style={styles.itemText}>{item.name}</Text>
+        <Text style={styles.itemText}>{item.Product?.name}</Text>
         <Text style={styles.itemPrice}>
-          {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}đ
+          {item.Product?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}đ
         </Text>
       </View>
       <View style={styles.status}>
-        <TouchableOpacity
+        {/* <TouchableOpacity
           style={styles.review}
-          onPress={() =>
-            navigation.navigate({
-              name: 'ReviewScreen'
-            })
-          }>
+          onPress={() => navigation.navigate({
+            name: 'ReviewScreen'
+          })}>
           <Text style={styles.buttonText}>Đánh giá</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.statusText}>
+        </TouchableOpacity> */}
+        {/* <TouchableOpacity style={styles.statusText}>
           <Text style={styles.buttonText}>Mua lại</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
     </View>
   );
 
   return (
     <View style={styles.container}>
-      <FlatList data={cartItems} renderItem={renderItem} />
+      {
+        isLoading ?
+          (
+            <LoaderKit
+              style={{ width: 45, height: 45, alignSelf: 'center' }}
+              name={'BallPulse'}
+              color={'green'}
+            />
+          )
+          :
+          data && data.length > 0 ?
+            < FlatList data={data} renderItem={renderItem} />
+            : <Text style={{ alignSelf: 'center', marginTop: 10 }}>Không có đơn cần đánh giá nào cả!</Text>
+      }
     </View>
   );
 };
--- Mobile/src/components/OrderStatus/Waiting_delivery.tsx
+++ Mobile/src/components/OrderStatus/Waiting_delivery.tsx
@@ -9,49 +9,90 @@ import {
   StyleSheet,
   Modal,
   Pressable,
+  TouchableOpacity
 } from 'react-native';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import LoaderKit from 'react-native-loader-kit';
+import api from '../../api/request';
 
 const Waiting_confirmation = () => {
-  const [cartItems, setCartItems] = useState([
-    {
-      id: 1,
-      name: 'Nem chua Thanh Hoá',
-      price: 150000,
-      Files: [{ src: 'https://i.pinimg.com/564x/6a/9a/12/6a9a122a60a435725152db7a6632da58.jpg' }],
-    },
-    {
-      id: 2,
-      name: 'Gạo đen Tây Bắc',
-      price: 300000,
-      Files: [{ src: 'https://i.pinimg.com/736x/8d/98/1e/8d981eadabf77f64baad46aac7279241.jpg' }],
-    },
-    {
-      id: 2,
-      name: 'Gạo đen Tây Bắc',
-      price: 300000,
-      Files: [{ src: 'https://i.pinimg.com/736x/8d/98/1e/8d981eadabf77f64baad46aac7279241.jpg' }],
-    },
-  ]);
+  const queryClient = useQueryClient();
+  const [idUpdate, setIdUpdate] = useState('');
 
-  const renderItem = ({ item }:any) => (
+  const { data, isLoading } = useQuery({
+    queryKey: ['get_order_CHO_GIAO_HANG'],
+    queryFn: async () => {
+      const res = await api.get('orders', { params: { status: "CHO_GIAO_HANG" } });
+      if (res) {
+        return res.data?.data;
+      }
+    },
+  });
+
+  const mutation = useMutation({
+    mutationKey: ['customer_received_order'],
+    mutationFn: async (id: string) => (await api.patch(`orders/${id}`, { params: { status: 'CHO_DANH_GIA' } })).data?.data,
+    onSuccess: async (data, vari) => {
+      await queryClient.invalidateQueries({
+        queryKey: ['get_order_CHO_GIAO_HANG'],
+      });
+      await queryClient.invalidateQueries({
+        queryKey: ['get_order_CHO_DANH_GIA'],
+      });
+    }
+  })
+
+  const renderItem = ({ item }: any) => (
     <View key={item.id} style={styles.itemContainer}>
-      <Image source={{ uri: item.Files?.[0]?.src }} style={styles.itemImage} />
+      <Image source={{ uri: item.Product?.Files?.[0]?.src }} style={styles.itemImage} />
       <View style={styles.content}>
-        <Text style={styles.itemText}>{item.name}</Text>
-        <Text style={styles.itemPrice}>{item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}đ</Text>
-
+        <Text style={styles.itemText}>{item.Product?.name}</Text>
+        <Text style={styles.itemPrice}>{item.Product?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}đ</Text>
       </View>
       <View style={styles.status}>
         <Text style={styles.update}>Đang giao</Text>
-        <Text style={styles.statusText}>Đã nhận</Text>
-        
+        <TouchableOpacity
+          disabled={mutation.isPending ? true : false}
+          onPress={() => mutation.mutate(item.id)}
+        >
+          <Text style={styles.statusText}>Đã nhận</Text>
+
+        </TouchableOpacity>
+
       </View>
-    </View>
+    </View >
   );
 
   return (
     <View style={styles.container}>
-      <FlatList data={cartItems} renderItem={renderItem} />
+      {
+        mutation.isPending
+          ?
+          (
+            <View style={{ flexDirection: 'row', alignSelf: 'center', alignItems: 'center', justifyContent: 'center', }}>
+              <Text style={{ color: 'green' }}>Đang xác nhận</Text>
+              <LoaderKit
+                style={{ width: 35, height: 20, alignSelf: 'center' }}
+                name={'BallPulse'}
+                color={'green'}
+              />
+            </View>)
+          : null
+      }
+      {
+        isLoading ?
+          (
+            <LoaderKit
+              style={{ width: 45, height: 45, alignSelf: 'center' }}
+              name={'BallPulse'}
+              color={'green'}
+            />
+          )
+          :
+          data && data.length > 0 ?
+            < FlatList data={data} renderItem={renderItem} />
+            : <Text style={{ alignSelf: 'center', marginTop: 10 }}>Không có đơn chờ giao hàng nào cả!</Text>
+      }
     </View>
   );
 };
@@ -81,29 +122,29 @@ const styles = StyleSheet.create({
     marginTop: 15,
   },
   itemImage: {
-    width:70,
+    width: 70,
     height: 70,
     borderRadius: 10,
-    marginRight:10,
+    marginRight: 10,
   },
   status: {
     flexDirection: 'column',
     alignItems: 'flex-end',
   },
-  statusText:{
+  statusText: {
     fontSize: 13,
     backgroundColor: '#2E7D32',
-    color:'white',
-    margin:7,
-    width:50,
-    height:20,
-    borderRadius:5,
-    textAlign:'center',
+    color: 'white',
+    margin: 7,
+    width: 50,
+    height: 20,
+    borderRadius: 5,
+    textAlign: 'center',
   },
   update: {
     fontSize: 13,
     color: '#2E7D32',
-    margin:7,
+    margin: 7,
   },
 });
 
