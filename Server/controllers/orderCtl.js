521fbab265a94704accfa893b9ef9d4a6117e0e7
thibackend <thi.a24technology@gmail.com>
2024-03-24 09:53:04 +0700

    VND-78-online-payment-via-stripe <Payment with stripe done>

--- Server/controllers/orderCtl.js
+++ Server/controllers/orderCtl.js
@@ -3,14 +3,16 @@ const {
     getAllOrder,
     sellerGetAllOrder,
     commonGetAllOrder,
-    updateOrderStatus } = require('../services/oderServices');
+    updateOrderStatus,
+    removeOrderServices
+} = require('../services/oderServices');
 
 const create = (req, res, next) => createNewOrder(req, res, next);
 const commonGetAll = (req, res, next) => commonGetAllOrder(req, res, next);
 const getAll = (req, res, next) => getAllOrder(req, res, next);
 const sellerGetAll = (req, res, next) => sellerGetAllOrder(req, res, next);
 const updateStatus = (req, res, next) => updateOrderStatus(req, res, next);
-
+const removeOrder = (req, res, next) => removeOrderServices(req, res, next);
 
 
 module.exports = {
@@ -18,5 +20,6 @@ module.exports = {
     create,
     getAll,
     sellerGetAll,
-    updateStatus
+    updateStatus,
+    removeOrder
 }
